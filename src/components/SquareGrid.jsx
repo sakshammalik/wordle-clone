0f197fd3bbcb99a16e86abe9c24a9ddc3ca4e841
Saksham <maliksaksham098@gmail.com>
2022-01-23 17:59:36 +0530

    created grid

--- src/components/SquareGrid.jsx
+++ src/components/SquareGrid.jsx
@@ -1,8 +1,27 @@
+import React from "react";
+import SquareGridElement from "./SquareGridElement";
+
+const GRID = [
+
+]; 
+
+
 const SquareGrid = () => {
+
+    const [grid, setGrid] = React.useState();
+
     return (
+      <div className="grid-container">
         <div className="square-grid">
-            Grid
+          {[0, 1, 2, 3, 4, 5].map(() => (
+            <div className="square-grid-row">
+              {[0, 1, 2, 3, 4].map(() => (
+                <SquareGridElement />
+              ))}
+            </div>
+          ))}
         </div>
+      </div>
     );
 };
 
---
+++ src/components/SquareGridElement.jsx
@@ -0,0 +1,7 @@
+const SquareGridElement = () => {
+    return (
+        <div className="square-grid-element"></div>
+    );
+};
+
+export default SquareGridElement;
